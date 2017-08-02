2da82333b14ace7b55c9ca601b5b392c731282b1
Xuqing Liu <ciwei100000@gmail.com>
2017-08-02 15:22:28 +0800

    Delete test3.sh

--- keywordlookup/test3.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-python keywordlookup_file.py -i '~/venv/lib/python2.7/site-packages/flask_appbuilder/' -o output_js_app_1 -fe '\.pyc$|^([^.]+)$|.png$|.gz' -pe '/assets/(images)/|/translations/' -pf './test_iput'
