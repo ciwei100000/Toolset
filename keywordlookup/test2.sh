3b2c8b9333c0d3cfd131cd4dd1eebbe34086f93b
Xuqing Liu <ciwei100000@gmail.com>
2017-08-02 14:59:43 +0800

    Update test2.sh

--- keywordlookup/test2.sh
+++ keywordlookup/test2.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-python keywordlookup_file.py -i '/home/liushu/PycharmProjects/incubator-superset/superset/' -o output_js_1 -fe '\.pyc$|^([^.]+)$|.png$|.gz' -pe '/assets/(images|dist|node_modules)/|/translations/' -pf './test_iput'
+python keywordlookup_file.py -i '~/PycharmProjects/incubator-superset/superset/' -o output_js_1 -fe '\.pyc$|^([^.]+)$|.png$|.gz' -pe '/assets/(images|dist|node_modules)/|/translations/' -pf './test_iput'
