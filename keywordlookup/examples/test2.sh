eb166dcb5164ef93a7f24f53469c9da0af34f9e6
Xuqing Liu <ciwei100000@gmail.com>
2017-08-02 17:32:04 +0800

    Update examples

--- keywordlookup/examples/test2.sh
+++ keywordlookup/examples/test2.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 
-<<<<<<< HEAD:keywordlookup/examples/test2.sh
-python keywordlookup_file.py -i '/home/liushu/PycharmProjects/incubator-superset/superset/' -o output_js_1 -fe '\.pyc$|^([^.]+)$|.png$|.gz' -pe '/assets/(images|dist|node_modules)/|/translations/' -pf '../test_iput'
-=======
 python keywordlookup_file.py -i '~/PycharmProjects/incubator-superset/superset/' -o output_js_1 -fe '\.pyc$|^([^.]+)$|.png$|.gz' -pe '/assets/(images|dist|node_modules)/|/translations/' -pf './test_iput'
->>>>>>> f6986cdccca64fcfb5f2b8c29a9b262f031e9dfd:keywordlookup/test2.sh
+
--- keywordlookup/examples/test3.sh
+++ keywordlookup/examples/test3.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 
-<<<<<<< HEAD:keywordlookup/examples/test3.sh
-python keywordlookup_file.py -i '/home/liushu/venv/lib/python2.7/site-packages/flask_appbuilder/' -o output_js_app_1 -fe '\.pyc$|^([^.]+)$|.png$|.gz' -pe '/assets/(images)/|/translations/' -pf '../test_iput'
-=======
 python keywordlookup_file.py -i '~/venv/lib/python2.7/site-packages/flask_appbuilder/' -o output_js_app_1 -fe '\.pyc$|^([^.]+)$|.png$|.gz' -pe '/assets/(images)/|/translations/' -pf './test_iput'
->>>>>>> f6986cdccca64fcfb5f2b8c29a9b262f031e9dfd:keywordlookup/test3.sh
+
