2163a00f57ab278312ffefab871b3ffc7e96c861
Xuqing Liu <ciwei100000@gmail.com>
2017-08-02 15:22:05 +0800

    Delete test.sh

--- keywordlookup/test.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-python keywordlookup.py -i '~/PycharmProjects/incubator-superset/superset' -o output_superset -fe '\.pyc$|^([^.]+)$|.png$|.gz' -pe '/assets/(node_modules|images|dist)/|/translations/'
-python keywordlookup.py -i '~/PycharmProjects/incubator-superset/superset' -o output_superset -fe '\.pyc$|^([^.]+)$|.png$|.gz' -pe '/assets/(node_modules|images|dist)/|/translations/'
-cat output_superset > output
-cat output_appbuilder >> output
